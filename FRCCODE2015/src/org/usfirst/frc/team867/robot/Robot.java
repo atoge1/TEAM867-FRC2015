ba55a8825009b453ce5c13843c33a30784e53087
atoge1 <atoge888@gmail.com>
2015-02-10 16:43:01 -0800

    Add more stuff
    finished adding preferences to joystick buttons (buttons are softcoded,
    axes are hard coded) also moved driver functions into a drive() method

--- FRCCODE2015/src/org/usfirst/frc/team867/robot/Robot.java
+++ FRCCODE2015/src/org/usfirst/frc/team867/robot/Robot.java
@@ -12,32 +12,59 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends IterativeRobot 
 {
-	RobotDrive myRobotForward; //robotdrive forward
+	RobotDrive myRobotForward; //robotdrive forward -- JTTR_BUG
 	Joystick driverJoy; //driver gamepad
 	BuiltInAccelerometer accel; // builtin accel
 	Preferences prefs = Preferences.getInstance(); //preferences menu
 	double driverJoyxaxis; // driver gamepad left x axis
 	double driverJoyyaxis; // driver gamepad left y axis
 	double driverJoyslow; // driver gamepad z axis (set speed)
-	boolean driverJoysafety; //safety trigger (1)
+	boolean driverJoygo; //safety trigger (1)
 	boolean driverJoyccw; //pivot counter clockwise (4)
 	boolean driverJoycw; //pivot clockwise (5)
-	boolean driverJoyreverse; //reverse directions (forward is now backwards)(2)
+	boolean driverJoyreverse; //reverse directions (forward is now backwards) (2)
 	boolean driveReverse; //store reverse toggle
-	double driveRotation;
-	double driveSlow;
-	
-	
+	double driveRotation; //rotation for the robot
+	double driveSlow; //robot speed 
 	
 	
     public void robotInit() //initialization code; period independent
     { 
-    	prefs.putInt("frontleft", 2);
-    	prefs.putInt("rearleft", 1);
-    	prefs.putInt("frontright", 3);
-    	prefs.putInt("rearright", 0);
+    	//print preferences table
+    	
+    	//motor ports
+    	prefs.putInt("MOTORfrontleft", 2);
+    	prefs.putInt("MOTORrearleft", 1);
+    	prefs.putInt("MOTORfrontright", 3);
+    	prefs.putInt("MOTORrearright", 0);
+    	
+    	//driverjoy 
+    	prefs.putInt("DRIVERreverse", 2);
+    	prefs.putInt("DRIVERgo", 1);
+    	prefs.putInt("DRIVERccw", 4);
+    	prefs.putInt("DRIVERcw", 5);
+    	
+    	
+    	//initialize joysticks
     	driverJoy = new Joystick(0);
+    	
+    	//initialize joystick inputs
+    	driverJoyccw = driverJoy.getRawButton(prefs.getInt("DRIVERccw", 4));
+		driverJoycw = driverJoy.getRawButton(prefs.getInt("DRIVERcw", 5));
+    	driverJoygo = driverJoy.getRawButton(prefs.getInt("DRIVERgo",1));
+    	driverJoyreverse = driverJoy.getRawButton(prefs.getInt("DRIVERreverse", 2));
+    	driverJoyxaxis = driverJoy.getRawAxis(0);
+		driverJoyyaxis = driverJoy.getRawAxis(1);
+		
+		
+    	
+		
+    	//initialize motors
     	myRobotForward = new RobotDrive(prefs.getInt("frontleft", 2), prefs.getInt("rearleft", 1), prefs.getInt("frontright", 3), prefs.getInt("rearright", 0)); //frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor
+    	
+    	
+    	
+    	
     	accel = new BuiltInAccelerometer();
     }
 
@@ -46,7 +73,7 @@ public class Robot extends IterativeRobot
 
     }
     
-    public void autonomousPeriodic() //autonomous period 
+    public void autonomousPeriodic() //autonomous period (loops)
     {
 
     }
@@ -56,98 +83,61 @@ public class Robot extends IterativeRobot
     	driveReverse=false;
     }
     
-    public void teleopPeriodic() //teleoperated period
+    public void teleopPeriodic() //teleoperated period (loops)
     {
-    	
-    	SmartDashboard.putNumber("Zaxis", accel.getZ());
-    	
-    	//"slow" factor
-    	driverJoyslow = driverJoy.getRawAxis(2);
-    	driveSlow = ((driverJoyslow + 1) * 4.5) + 1;
-    	
-    	//reverse toggle
-    	driverJoyreverse = driverJoy.getRawButton(2);
-    	if(driverJoyreverse)
+    	//drive when go button is held
+    	if(driverJoygo)
     	{
-    		driveReverse = !driveReverse;
-    		Timer.delay(0.3);
-    		
-    	}
-    	SmartDashboard.putBoolean("driveReverse", driveReverse);
-    	
-    	//safety
-    	driverJoysafety = driverJoy.getRawButton(1);
-    	SmartDashboard.putBoolean("driverJoysafety", driverJoysafety);
-    	
-    	
-    	//forward driving
-    	if(!driveReverse && driverJoysafety)
-    	{
-    		driverJoyxaxis = driverJoy.getRawAxis(0);
-    		driverJoyyaxis = driverJoy.getRawAxis(1);
-    		driverJoyccw = driverJoy.getRawButton(4);
-    		driverJoycw = driverJoy.getRawButton(5);
-    		
-    		//rotation
-    		if(driverJoyccw)
-    		{
-    			driveRotation = -0.5;
-    		}
-    		else if(driverJoycw)
-    		{
-    			driveRotation = 0.5;
-    		}
-    		else
-    		{
-    			driveRotation = 0.0;
-    		}
-    		
-    		//print out drive values
-    		SmartDashboard.putNumber("driveSlow", 10-driveSlow);
-    		SmartDashboard.putNumber("driverJoyxaxis", driverJoyxaxis / driveSlow);
-    		SmartDashboard.putNumber("driverJoyyaxis", -1 * driverJoyyaxis / driveSlow);
-    		SmartDashboard.putBoolean("driverJoycw", driverJoycw);
-   			SmartDashboard.putBoolean("driverJoyccw", driverJoyccw);
-   			myRobotForward.mecanumDrive_Cartesian(driverJoyxaxis / driveSlow , driverJoyyaxis / driveSlow, driveRotation, 0);
-    		
-    		
-    		
+    		drive();
     	}
     	
-    	//reverse driving
-    	if(driveReverse && driverJoysafety)
-    	{
-    		driverJoyxaxis = driverJoy.getRawAxis(0);
-    		driverJoyyaxis = driverJoy.getRawAxis(1);
-    		driverJoyccw = driverJoy.getRawButton(4);
-    		driverJoycw = driverJoy.getRawButton(5);
-    		
-    		//rotation
-    		if(driverJoyccw)
-    		{
-    			driveRotation = -0.5;
-    		}
-    		else if(driverJoycw)
-    		{
-    			driveRotation = 0.5;
-    		}
-    		else
-    		{
-    			driveRotation = 0.0;
-    		}
-    		
-    		SmartDashboard.putNumber("driveSlow", 10-driveSlow);
-    		SmartDashboard.putNumber("driverJoyxaxis", driverJoyxaxis / driveSlow);
-    		SmartDashboard.putNumber("driverJoyyaxis", -1 * driverJoyyaxis / driveSlow);
-   			SmartDashboard.putBoolean("driverJoyccw", driverJoyccw);
-   			myRobotForward.mecanumDrive_Cartesian( -1 * driverJoyxaxis / driveSlow, -1 * driverJoyyaxis / driveSlow, driveRotation, 0);
-    		
-    	}
-        
+    	
+    	
+    	
     }
     
     public void testPeriodic() //test period
     {
+    	//left blank on purpose
     }
     
+    private void drive()
+    {
+    	//"slow" factor
+    	driverJoyslow = driverJoy.getRawAxis(2);
+    	driveSlow = ((driverJoyslow + 1) * 4.5) + 1;
+    	    	
+    	//reverse toggle
+    	if(driverJoyreverse)
+    	{
+    		driveReverse = !driveReverse;
+    		Timer.delay(0.3);
+    	}
+    	
+    	//rotation
+		if(driverJoyccw)
+		{
+			driveRotation = -0.5;
+		}
+		else if(driverJoycw)
+		{
+			driveRotation = 0.5;
+		}
+		else
+		{
+			driveRotation = 0.0;
+		}
+
+    	//forward driving
+    	if(!driveReverse)
+    	{
+   			myRobotForward.mecanumDrive_Cartesian(driverJoyxaxis / driveSlow , driverJoyyaxis / driveSlow, driveRotation, 0);    		
+    	}
+    	
+    	//reverse driving
+    	if(driveReverse)
+    	{    		
+    		myRobotForward.mecanumDrive_Cartesian( -1 * driverJoyxaxis / driveSlow, -1 * driverJoyyaxis / driveSlow, driveRotation, 0);
+    	}
+    }
 }
